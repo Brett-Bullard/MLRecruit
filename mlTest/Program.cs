363ea3bfaff8e479a8a434ea52976a5e1962ba17
Brett Bullard <brett.bullard@cbsi.com>
2018-12-06 10:29:35 -0600

    Intial algorithm

--- mlTest/Program.cs
+++ mlTest/Program.cs
@@ -33,68 +33,83 @@ namespace myApp
 
         static void Main(string[] args)
         {
-               // STEP 2: Create a ML.NET environment  
-                 var mlContext = new MLContext();
+            Console.WriteLine("Would you like to load the new model (1) or run the existing model (0)?");
+            bool loadModel = Convert.ToBoolean(Console.ReadLine());
+            if (loadModel)
+            {
+                // STEP 2: Create a ML.NET environment  
+                var mlContext = new MLContext();
 
-                 // If working in Visual Studio, make sure the 'Copy to Output Directory'
-                 // property of iris-data.txt is set to 'Copy always'
-                 string dataPath = "iris-data.txt";
-                 var reader = mlContext.Data.TextReader(new TextLoader.Arguments()
-                 {
-                     Separator = ",",
-                     HasHeader = false,
-                     Column = new[]
-                     {
-                         new TextLoader.Column("Lat", DataKind.R4, 0),
-                         new TextLoader.Column("Lng", DataKind.R4, 1),
-                         new TextLoader.Column("Rating", DataKind.R4, 2),
-                         new TextLoader.Column("Label", DataKind.Text, 3),
-                     }
-                 });
+                // If working in Visual Studio, make sure the 'Copy to Output Directory'
+                // property of iris-data.txt is set to 'Copy always'
+                string dataPath = "iris-data.txt";
+                var reader = mlContext.Data.TextReader(new TextLoader.Arguments()
+                {
+                    Separator = ",",
+                    HasHeader = false,
+                    Column = new[]
+                    {
+                        new TextLoader.Column("Lat", DataKind.R4, 0),
+                        new TextLoader.Column("Lng", DataKind.R4, 1),
+                        new TextLoader.Column("Rating", DataKind.R4, 2),
+                        new TextLoader.Column("Label", DataKind.Text, 3),
+                    }
+                });
 
 
-                 IDataView trainingDataView = reader.Read(new MultiFileSource(dataPath));
+                IDataView trainingDataView = reader.Read(new MultiFileSource(dataPath));
 
-                 // STEP 3: Transform your data and add a learner
-                 // Assign numeric values to text in the "Label" column, because only
-                 // numbers can be processed during model training.
-                 // Add a learning algorithm to the pipeline. e.g.(What type of iris is this?)
-                 // Convert the Label back into original text (after converting to number in step 3)
-                 var pipeline = mlContext.Transforms.Conversion.MapValueToKey("Label")
-                     .Append(mlContext.Transforms.Concatenate("Features", "Lat", "Lng", "Rating"))
-                     .Append(mlContext.MulticlassClassification.Trainers.LogisticRegression(labelColumn: "Label", featureColumn: "Features"))
-                     .Append(mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
-                 // STEP 4: Train your model based on the data set  
-                 var model = pipeline.Fit(trainingDataView);
+                // STEP 3: Transform your data and add a learner
+                // Assign numeric values to text in the "Label" column, because only
+                // numbers can be processed during model training.
+                // Add a learning algorithm to the pipeline. e.g.(What type of iris is this?)
+                // Convert the Label back into original text (after converting to number in step 3)
 
-                 using (var stream = File.Create("D:\\code\\LogisticBigModel.zip"))
-                 {
-                     // Saving and loading happens to 'dynamic' models.
-                     mlContext.Model.Save(model, stream);
-                 }
-                 
+                var pipeline = mlContext.Transforms.Conversion.MapValueToKey("Label")
+                    .Append(mlContext.Transforms.Concatenate("Features", "Lat", "Lng", "Rating"))
+                    .Append(mlContext.Ranking.Trainers.FastTree(labelColumn: "Label", featureColumn: "Features"))
+                    .Append(mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
+                // STEP 4: Train your model based on the data set  
+                var model = pipeline.Fit(trainingDataView);
+
+                using (var stream = File.Create("D:\\code\\LogisticBigModel_TN_FastTree.zip"))
+                {
+                    // Saving and loading happens to 'dynamic' models.
+                    mlContext.Model.Save(model, stream);
+                }
+            }
             // STEP 5: Use your model to make a prediction
             // You can change these numbers to test different predictions
-      /*      var mlContext = new MLContext();
+            LoadAndRunModel();
+
+        }
+
+        private static void LoadAndRunModel()
+        {
+            var mlContext = new MLContext();
             ITransformer model;
-            using (var stream = File.OpenRead("D:\\code\\LogisticBigModel.zip"))
-                model = mlContext.Model.Load(stream);*/
+            using (var stream = File.OpenRead("D:\\code\\LogisticBigModel_TN_FastTree.zip"))
+                model = mlContext.Model.Load(stream);
             while (1 == 1)
             {
-                Console.WriteLine("What rating does this guy have?");
-                int rating = Convert.ToInt32(Console.ReadLine());
-                
-                var prediction = model.MakePredictionFunction<RecruitData, RecruitLabel>(mlContext).Predict(
-                    new RecruitData()
-                    {
-                        Lat = (float)40.1495,
-                        Lng = (float)82.4003,
-                        Rating = rating,
-                    });
 
-                Console.WriteLine($"Predicted school  is: {prediction.RecruitPrediction}");
+                Predict(mlContext, model);
             }
-            
+        }
+
+        public static void Predict(MLContext mlContext, ITransformer model)
+        {
+            Console.WriteLine("What rating does this guy have?");
+            int rating = Convert.ToInt32(Console.ReadLine());
+            var predictionFunction = model.MakePredictionFunction<RecruitData, RecruitLabel>(mlContext); 
+            var prediction =predictionFunction.Predict(
+                new RecruitData()
+                {
+                    Lat = (float)36.1495,
+                    Lng = (float)86.4003,
+                    Rating = rating,
+                });
+            Console.WriteLine($"Predicted school  is: {prediction.RecruitPrediction}");
         }
     }
 }
